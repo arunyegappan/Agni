ce89a11b4487fe0f7924c56455f5b2b895be99ac
Arun Ram <y.arunram@gmail.com>
2013-06-24 16:03:16 +0530

    Agni

---
+++ graffiti.php
@@ -0,0 +1,31 @@
+<html>
+<title>AGNI</title>
+<head>
+<?php
+include("head.txt");
+?>
+
+</head>
+
+<body>
+<?php
+if(!($_GET['click']))
+{
+include ("top.txt");
+echo '<div id="fullcontent">';
+}
+
+include("graffiti.txt");
+
+if(!($_GET['click']))
+{
+
+echo '</div>';
+include ("sideupdates.txt");
+include("footer.txt");
+}
+?>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<title>AGNI</title>
+<head>
+<?php
+include("head.txt");
+?>
+
+</head>
+
+<body>
+<?php
+if(!($_GET['click']))
+{
+include ("top.txt");
+echo '<div id="fullcontent">';
+}
+
+include("homeajax.txt");
+
+
+if(!($_GET['click']))
+{
+echo "</div>";
+include ("sideupdates.txt");
+
+include("footer.txt");
+}
+?>
+</div>
+
+</body>
+</html>
